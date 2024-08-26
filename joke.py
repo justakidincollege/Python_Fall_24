bc5dae6a9e50b84d06a893fe5e08872aa490ef1c
justakidincollege <qphan@students.mchenry.edu>
2024-08-26 13:49:48 -0500

    Add files via upload

---
+++ joke.py
@@ -0,0 +1,4 @@
+#Assingment 1
+#Knock Knock Jokes
+#one line of code to write the entire joke on sperate lines using esacape characters
+print("Knock, knock", "\nWho's there?", "\nDUCK!", "\nWha-", "\n*Gets assaulted by a duck", "\nI told you to duck")
