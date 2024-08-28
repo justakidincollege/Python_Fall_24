8d7ae2be59c09d6a97fc144f7294754b400c4a1e
justakidincollege <qphan@students.mchenry.edu>
2024-08-28 13:41:25 -0500

    Add files via upload

---
+++ math_operations.py
@@ -0,0 +1,11 @@
+# Math operations
+
+# Variables
+length=14
+width=11
+
+# Operation
+area=length*width
+
+# Display Message
+print("The area of the rectangle with a length of", length, "and a width of", width, "is", area)
