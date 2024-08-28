9a998411f03fbad4e3bd2acc20c3a94d63df2c16
justakidincollege <qphan@students.mchenry.edu>
2024-08-28 14:04:34 -0500

    Add files via upload

---
+++ kilograms_conversions.py
@@ -0,0 +1,39 @@
+# Kilograms to Pounds Conversion
+
+# Variables
+kilograms=10 
+conversion_factor=2.20462
+
+# Converion
+pounds_1=kilograms*conversion_factor
+
+# Change value of kilograms
+kilograms=15
+
+# Conversion
+pounds_2=kilograms*conversion_factor
+
+# Change value of kilograms 
+kilograms=20
+
+# Conversion
+pounds_3=kilograms*conversion_factor
+
+# Change value of kilograms
+kilograms=25
+
+# Conversion
+pounds_4=kilograms*conversion_factor
+
+# Change value of kilograms
+kilograms=30
+
+# Conversion
+pounds_5=kilograms*conversion_factor
+
+# Display Convsersions
+print("Conversion 1:",pounds_1,"pounds")
+print("Conversion 2:",pounds_2,"pounds")
+print("Conversion 3:",pounds_3,"pounds")
+print("Conversion 4:",pounds_4,"pounds")
+print("Conversion 5:",pounds_5,"pounds")
