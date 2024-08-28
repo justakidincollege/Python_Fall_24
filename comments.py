0484b30124c542969a75376e48291653b87217ed
justakidincollege <qphan@students.mchenry.edu>
2024-08-28 14:10:15 -0500

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,11 @@
+# Comments for Sample Python Program
+
+# Variables
+tank_size = 15
+distance = 600
+
+# Mathematical Operation to find miles per gallon
+mpg = distance / tank_size
+
+# Displaying the results of the matematical operation
+print("Miles per gallon:", mpg)
