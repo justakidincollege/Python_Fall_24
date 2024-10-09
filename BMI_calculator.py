af1637bc9596360ddad7f94447745f54aa0f3f7d
justakidincollege <qphan@students.mchenry.edu>
2024-10-09 16:46:39 -0500

    Add files via upload

---
+++ BMI_calculator.py
@@ -0,0 +1,37 @@
+# BMI_calculator assignment
+# Inform user on what the program does
+print("Hello user I am a BMI caluclator.")
+
+# Start functions to convert variables
+def calculate_kilograms(WEIGHT):
+    global KILOGRAMS
+    KILOGRAMS=WEIGHT*0.45359
+    return KILOGRAMS
+def calculate_meters(HEIGHT):
+    global METERS
+    METERS=HEIGHT*0.0254
+    return HEIGHT
+
+# Calculate BMI
+def calculate_BMI():
+    global BMI
+    BMI=KILOGRAMS/(METERS*METERS)
+# Start the main part of the program
+def main():
+    global HEIGHT
+    HEIGHT=float(input("Please enter your height in inches: "))
+    global WEIGHT
+    WEIGHT=float(input("Please enter your weight in pounds: "))
+    calculate_kilograms(WEIGHT)
+    calculate_meters(HEIGHT)
+    calculate_BMI()
+    print(f"Your BMI is {BMI:.2f}.")
+    if BMI<18.5:
+        print("You are in the underweight category")
+    elif BMI<24.9:
+        print("You are in the normal weight category.")
+    elif BMI<29.9:
+        print("You are in the overweight category.")
+    elif BMI>=30:
+        print("You are in the obese category.")
+main()
