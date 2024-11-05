b4d9b35bc3fbc706dfedae1698d3c355ca2ee2ab
justakidincollege <qphan@students.mchenry.edu>
2024-11-04 23:36:37 -0600

    Add files via upload

---
+++ string_methods.py
@@ -0,0 +1,36 @@
+# String methods practice assignment
+# completed tasks with my own code
+string1="python"
+print(string1.capitalize())
+print(string1.center(10))
+print(string1.endswith("on"))
+print(string1.find("th"))
+string2="python3"
+print(string2.isalnum())
+print(string1.isalpha())
+print(string1.islower())
+string3="    "
+print(string3.isspace())
+string4="PYTHON"
+print(string4.isupper())
+iterable1 = ["Python", "is", "fun"]
+separator = "-"
+print(separator.join(iterable1))
+print(string4.lower())
+string5="               python"
+print(string5.lstrip())
+string6="python               "
+print(string6.rstrip())
+string7="I love python"
+print(string7.replace("python","snake"))
+print(string1.rfind("o"))
+string8="python-is-fun"
+print(string8.split("-"))
+print(string1.startswith("py"))
+string9="   python   "
+print(string9.strip())
+string10="Python"
+print(string10.swapcase())
+string11="python is fun"
+print(string11.title())
+print(string1.upper())
