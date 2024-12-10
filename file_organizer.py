6874569be8fa3faadeae742f602d31c4cdb25357
justakidincollege <qphan@students.mchenry.edu>
2024-12-09 23:10:45 -0600

    Add files via upload

---
+++ file_organizer.py
@@ -0,0 +1,10 @@
+# File Organizer Assignment
+# Import OS
+import os
+# Create main function
+def main():
+    os.mkdir("MyFiles")
+    os.mkdir("MyFiles/Docs")
+    os.mkdir("MyFiles/Images")
+    os.mkdir("MyFiles/Videos")
+main()
